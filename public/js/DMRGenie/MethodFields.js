84992205dd7a6906c63f3bfce722a6e12f3acb2a
Thomas E. Baker <7773410+bakerte@users.noreply.github.com>
2024-12-29 00:30:38 -0800

    Delete public directory

--- public/js/DMRGenie/MethodFields.js
+++
@@ -1,9 +0,0 @@
-function SystemChange() {
-  var selected = document.getElementById("system");
-  var selectedValue = selected.options[selected.selectedIndex].value;
-  if (selectedValue == "input") {
-    document.getElementById("system_input_file").style.display = "inline";
-  } else {
-    document.getElementById("system_input_file").style.display = "none";
-  }
-}
